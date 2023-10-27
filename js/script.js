6dcabc4a6e67414f75509e012002611ba428b89d
DevendhiranE <everysupertips@gmail.com>
2023-10-27 11:54:51 +0530

    fuel-cost-edit

--- js/script.js
+++ js/script.js
@@ -10,6 +10,6 @@ calculateBtn.addEventListener("click", (e) => {
     let result =
       (tripDistanceEl.value / fuelEfficiencyEl.value) * fuelCostEl.value;
 
-    alert("Your Trip Distance is : Rs. " + result);
+    alert("Your Fuel Cost will be around : Rs. " + result);
   }
 });
